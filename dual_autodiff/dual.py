33cff83544e6f53e5bb563a19400ed0d38b69826
I.G. Faisal <igf23@cam.ac.uk>
2024-11-16 13:41:13 +0000

    Addition Feature

--- dual_autodiff/dual.py
+++ dual_autodiff/dual.py
@@ -6,3 +6,16 @@ class Dual:
         self.real = real
         self.dual = dual
     
+#We are using Python's magic methods to define how the Dual object's algebra works
+
+    def __add__(self, other):
+        if isinstance(other, Dual): #Dual + Dual
+            return Dual(self.real + other.real, self.dual + other.dual)
+        elif isinstance(other, (int,float)): #Dual + Real Number
+            return Dual(self.real + other, self.dual)
+        else:
+            return NotImplementedError
+    
+    def __radd__(self, other): # This just allows us to do c + dual (a,b) = dual(a+c, b) where c is a Real number
+        return self.__add__(other)
+    
